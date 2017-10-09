f52fbf87ad43ce94c6f8874b8f88d2700ebd0ea9
andino93 <andy.andino93@gmail.com>
2017-10-09 12:33:24 -0700

    added code for up to some

--- src/underbar.js
+++ src/underbar.js
@@ -7,6 +7,7 @@
   // seem very useful, but remember it--if a function needs to provide an
   // iterator when the user does not pass one in, this will be handy.
   _.identity = function(val) {
+    return val;
   };
 
   /**
@@ -37,6 +38,7 @@
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+    return _.first(array.reverse(), n);
   };
 
   // Call iterator(value, key, collection) for each element of collection.
@@ -45,6 +47,15 @@
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
+    if (Array.isArray(collection)) {
+      for (var i = 0; i < collection.length; i++) {
+        iterator(collection[i], i, collection);
+      }
+    } else {
+      for (var key in collection) {
+        iterator(collection[key], key, collection);
+      }
+    }
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
@@ -66,16 +77,45 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var arr = [];
+    _.each(collection, function(e) {
+      if (test(e)) {
+        arr.push(e);
+      } 
+    });
+    return arr;
   };
 
   // Return all elements of an array that don't pass a truth test.
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    return _.filter(collection, function(e) {
+      return !test(e);
+    });
   };
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array, isSorted, iterator) {
+    
+    var uniqArr = [];
+    if (isSorted) {
+      _.each(array, function(element) {
+        if (_.indexOf(uniqArr, element) === -1) {
+          if (iterator(element) || iterator(_.indexOf(array, element))) {
+            uniqArr.push(element);
+          }
+        }
+      });
+      
+      return uniqArr;
+    }
+    _.each(array, function(element) {
+      if (_.indexOf(uniqArr, element) === -1) {
+        uniqArr.push(element);
+      }
+    });
+    return uniqArr;
   };
 
 
@@ -84,6 +124,11 @@
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
+    var results = [];
+    _.each(collection, function(e) {
+      results.push(iterator(e));
+    });
+    return results;
   };
 
   /*
@@ -125,6 +170,14 @@
   //   }); // should be 5, regardless of the iterator function passed in
   //          No accumulator is given so the first element is used.
   _.reduce = function(collection, iterator, accumulator) {
+    _.each(collection, function(element, index) {
+      if ((accumulator === undefined && index === 0)) {
+        accumulator = element;
+      } else {
+        accumulator = iterator(accumulator, element);
+      }
+    });
+    return accumulator;
   };
 
   // Determine if the array or object contains a given value (using `===`).
@@ -143,12 +196,26 @@
   // Determine whether all of the elements match a truth test.
   _.every = function(collection, iterator) {
     // TIP: Try re-using reduce() here.
+    iterator = iterator === undefined ? _.identity : iterator;
+
+    return !!_.reduce(collection, function(isTrue, element) {
+      if (!iterator(element)) { 
+        isTrue = 0;
+      }
+      return isTrue;
+    }, 1);
+    
   };
 
   // Determine whether any of the elements pass a truth test. If no iterator is
   // provided, provide a default one
   _.some = function(collection, iterator) {
     // TIP: There's a very clever way to re-use every() here.
+    iterator = iterator === undefined ? _.identity : iterator;
+
+    return !_.every(collection, function(element) {
+      return !iterator(element);
+    });
   };
 
 
